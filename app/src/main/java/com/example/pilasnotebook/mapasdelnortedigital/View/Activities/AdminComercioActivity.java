cd0a590c4e51631cf04759e2191726e6d6a20cd7
Unknown <sebastian.sdc@gmail.com>
2018-09-29 05:40:42 -0300

    volver a empezar !

---
+++ app/src/main/java/com/example/pilasnotebook/mapasdelnortedigital/View/Activities/AdminComercioActivity.java
@@ -0,0 +1,15 @@
+package com.example.pilasnotebook.mapasdelnortedigital.View.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.pilasnotebook.mapasdelnortedigital.R;
+
+public class AdminComercioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_comercio);
+    }
+}
